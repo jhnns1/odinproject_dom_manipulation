eb68a8487dc8e6bb0535ef86a839f310e8075364
jhnns1 <joheck1991@gmail.com>
2023-11-06 12:32:02 +0100

    added more DOM manipulation ex1 ex2

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Learnings
 
 DOM manipulation only affects the DOM, not the underlaying HTML.
+
+See <a href="https://www.theodinproject.com/lessons/foundations-dom-manipulation-and-events">DOM Manipulation</a> for reference.
--- app.js
+++ app.js
@@ -1,63 +1,16 @@
-const container = document.querySelector("#container");
+const link = document.querySelector("a");
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
 
-//add div
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hoped you enjoyed the ride";
 
-container.appendChild(content);
+sect.appendChild(para);
 
-// add paragraph
-const paragraph = document.createElement("p");
-paragraph.classList.add("paragraph");
-paragraph.textContent = "Hey I'm red!";
-paragraph.style.color = "red";
+const text = document.createTextNode(
+  " - the premier source for web dev knowledge."
+);
 
-container.appendChild(paragraph);
-
-//add h3
-const headline3 = document.createElement("h3");
-headline3.classList.add("h3");
-headline3.textContent = "I'm a blue h3!";
-headline3.style.color = "blue";
-
-container.appendChild(headline3);
-
-// add another div with a h1, p
-const content2 = document.createElement("div");
-const headline1 = document.createElement("h1");
-headline1.classList.add("h1");
-headline1.textContent = "I'm in a div";
-const paragraph2 = document.createElement("p");
-paragraph2.classList.add("p");
-paragraph2.textContent = "Me too!";
-
-content2.appendChild(headline1);
-content2.appendChild(paragraph2);
-
-content2.style.backgroundColor = "pink";
-content2.style.borderColor = "black";
-content2.style.borderStyle = "solid";
-
-container.appendChild(content2);
-
-// EVENTS
-const btn = document.querySelector("#btn");
-/* btn.addEventListener("click", () => {
-  alert("Hello World");
-}); */
-
-function alertFunction() {
-  alert("YAY! YOU DID IT!");
-}
-
-// Method 1
-// btn.onclick = alertFunction;
-
-// Method 2
-// btn.addEventListener("click", alertFunction);
-
-btn.addEventListener("click", function (e) {
-  //console.log(e.target);
-  e.target.style.background = "blue";
-});
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
